ca67b6256b696a147e5db6c47b39dda877313f89
Lorenzo Gravina <lorenzo.gravina@me.com>
2021-11-09 08:27:06 +0300

    finish base functionality

--- script.js
+++ script.js
@@ -1,13 +1,50 @@
+//@ts-check
 let numberOfCellsPerSide = 16;
 const container = document.getElementsByClassName('container')[0];
-let cells = [];
+const body = document.querySelector('body');
 
-for (let i = 0; i < numberOfCellsPerSide ** 2; i++) {
-  cells.push(document.createElement('div').classList.add('cell'));
+// create rows
+for (let i = 0; i < numberOfCellsPerSide; i++) {
+  const row = document.createElement('div');
+  row.classList.add('row');
+  container.appendChild(row);
 }
 
-for (const cell in cells) {
-  if (Object.hasOwnProperty.call(cells, cell)) {
-    container.appendChild(cell);
+// in each row, create cells
+let rows = document.getElementsByClassName('row');
+
+for (let i = 0; i < numberOfCellsPerSide; i++) {
+  for (let n = 0; n < numberOfCellsPerSide; n++) {
+    const cell = document.createElement('div');
+    cell.classList.add('cell');
+    rows[i].appendChild(cell);
   }
 }
+
+// change background color when mouseover
+const cells = document.getElementsByClassName('cell');
+for (let i = 0; i < cells.length; i++) {
+  cells[i].addEventListener('mouseover', (e) => {
+    e.target['classList'].add('hovered');
+  });
+}
+
+// add button to reset
+const buttonRow = document.createElement('div');
+buttonRow.classList.add('buttonRow');
+
+const resetButton = document.createElement('button');
+resetButton.classList.add('reset');
+resetButton.textContent = "Reset";
+buttonRow.appendChild(resetButton);
+
+body.appendChild(buttonRow);
+
+// add functionality to reset button
+resetButton.addEventListener('click', () => {
+  for (let i = 0; i < cells.length; i++) {
+    const cell = cells[i];
+    cell.classList.remove('hovered');
+  }
+})
+
