74946d96351b0ce8f3e58f48672535a93ad0f737
Lorenzo Gravina <lorenzo.gravina@me.com>
2021-11-09 17:10:04 +0300

    add ability for user to resize grid

--- script.js
+++ script.js
@@ -1,50 +1,72 @@
 //@ts-check
-let numberOfCellsPerSide = 16;
+let initialSideLength = 16;
 const container = document.getElementsByClassName('container')[0];
 const body = document.querySelector('body');
 
-// create rows
-for (let i = 0; i < numberOfCellsPerSide; i++) {
-  const row = document.createElement('div');
-  row.classList.add('row');
-  container.appendChild(row);
-}
+/**
+ * @param {number} sideLength
+ */
+function createGrid(sideLength) {
+  // delete existing grid
+  container.innerHTML = "";
 
-// in each row, create cells
-let rows = document.getElementsByClassName('row');
+  // create rows
+  for (let i = 0; i < sideLength; i++) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+    container.appendChild(row);
+  }
 
-for (let i = 0; i < numberOfCellsPerSide; i++) {
-  for (let n = 0; n < numberOfCellsPerSide; n++) {
-    const cell = document.createElement('div');
-    cell.classList.add('cell');
-    rows[i].appendChild(cell);
+  // in each row, create cells
+  let rows = document.getElementsByClassName('row');
+
+  for (let i = 0; i < sideLength; i++) {
+    for (let n = 0; n < sideLength; n++) {
+      const cell = document.createElement('div');
+      cell.classList.add('cell');
+      rows[i].appendChild(cell);
+    }
+  }
+  // change background color when mouseover
+  const cells = document.getElementsByClassName('cell');
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].addEventListener('mouseover', (e) => {
+      e.target['classList'].add('hovered');
+    });
   }
 }
 
-// change background color when mouseover
-const cells = document.getElementsByClassName('cell');
-for (let i = 0; i < cells.length; i++) {
-  cells[i].addEventListener('mouseover', (e) => {
-    e.target['classList'].add('hovered');
-  });
-}
+createGrid(initialSideLength);
 
 // add button to reset
 const buttonRow = document.createElement('div');
 buttonRow.classList.add('buttonRow');
+body.appendChild(buttonRow);
 
 const resetButton = document.createElement('button');
 resetButton.classList.add('reset');
 resetButton.textContent = "Reset";
 buttonRow.appendChild(resetButton);
 
-body.appendChild(buttonRow);
+const resizeButton = document.createElement('button');
+resizeButton.classList.add('resize');
+resizeButton.textContent = "Resize Grid";
+buttonRow.appendChild(resizeButton);
 
 // add functionality to reset button
 resetButton.addEventListener('click', () => {
+  const cells = document.getElementsByClassName('cell');
   for (let i = 0; i < cells.length; i++) {
     const cell = cells[i];
     cell.classList.remove('hovered');
   }
 })
 
+resizeButton.addEventListener('click', () => {
+  const newSize = Number(window.prompt("New side size:"));
+  if (newSize > 100) {
+    console.log("too high");
+  } else {
+    createGrid(newSize);
+  }
+})
